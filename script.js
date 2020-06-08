e26ba8506e5b75d51ae5340ce0ab137c3941d612
Josh Homer <joshuachomer@gmail.com>
2020-06-08 19:47:02 -0400

    updated functions, able to add user's password criteria to a concatenated string

--- script.js
+++ script.js
@@ -1,31 +1,90 @@
-
-
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?', ':', ',', '{', '}', '/', '`', '~', '-', '_', '=', '+', '.', "'"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  
+  
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-var wantSpecialCharacters = confirm('Click OK to confirm if you want special characters in your password.');
-var wantNumbers = confirm("Click 'OK' to confirm if you want numeric characters in your password.");
-var wantLowerCase = confirm("Click 'OK' to confirm if you want lowercase characters in your password.");
-var wantUpperCase = confirm("Click 'OK' to confirm if you want uppercase characters in your password.");
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
-var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?', ':', ',', '{', '}', '/', '`', '~', '-', '_', '=', '+', '.', "'"];
 
-var randomLower = lowerCase.charAt(Math.floor(Math.random() * 26));
-var randomUpper = upperCase.charAt(Math.floor(Math.random() * 26));
-var randomSymbol = symbols[(Math.floor(Math.random() * symbols.length))];
-var randomNumber = Math.floor(Math.random() * 10);
+function generatePassword() {
+  var wantSpecialCharacters = confirm('Click OK to confirm if you want special characters in your password.');
+  var wantLowerCase = confirm("Click 'OK' to confirm if you want lowercase characters in your password.");
+  var wantUpperCase = confirm("Click 'OK' to confirm if you want uppercase characters in your password.");
+  var wantNumbers = confirm("Click 'OK' to confirm if you want numeric characters in your password.");
+  
+  // if statement
+  // lowerCase.concat(upperCase, symbols, numbers);
+  var password = "";
+  var workingArray = "";
 
-var password = "";
+  function combinedArr() {
+    if (wantSpecialCharacters) {
+      workingArray = workingArray + specialCharacters.concat(workingArray);
+      console.log(workingArray);
+    }
+    if (wantLowerCase) {
+      workingArray = lowerCase.concat(workingArray);
+      console.log(workingArray)
+    }
+    if (wantUpperCase) {
+      workingArray = upperCase.concat(workingArray);
+    }
+    if (wantNumbers) {
+      numbers = numbers.toString();
+      workingArray = numbers.concat(workingArray);
+      console.log(workingArray);
+    }
+  } 
+
+  
+
+if (wantSpecialCharacters !== true && wantNumbers !== true && wantLowerCase !== true && wantUpperCase !== true) {
+  alert("You must select to include lease one in your password.");
+}
+  else {
+  var numOfCharacters = prompt("How many character would you like in your password? Please choose between 8-128 characters.");
+  }
+  
+  
+
+  
+  
+  
+    if (numOfCharacters > 7 && numOfCharacters < 129) {
+    
+      for (var i =0; i < numOfCharacters.length; i++) {
+        password = password + combinedArr(Math.floor[Math.random() * combinedArr.length]);
+        
+      }
+
+    }
+    else {
+      alert("Must contain between 8-128 characters.");
+    }
+    return password;
+  
+  
+ 
+//   }
+//   }
+//   }
+//   }
+}
+
