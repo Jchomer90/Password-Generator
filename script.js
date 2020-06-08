522a46cd26fa0636a9ac1267ebbc80746dc8898f
Josh Homer <joshuachomer@gmail.com>
2020-06-08 07:00:48 -0400

    Added variables needed

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -9,6 +11,21 @@ function writePassword() {
   passwordText.value = password;
 
 }
+var wantSpecialCharacters = confirm('Click OK to confirm if you want special characters in your password.');
+var wantNumbers = confirm("Click 'OK' to confirm if you want numeric characters in your password.");
+var wantLowerCase = confirm("Click 'OK' to confirm if you want lowercase characters in your password.");
+var wantUpperCase = confirm("Click 'OK' to confirm if you want uppercase characters in your password.");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?', ':', ',', '{', '}', '/', '`', '~', '-', '_', '=', '+', '.', "'"];
+
+var randomLower = lowerCase.charAt(Math.floor(Math.random() * 26));
+var randomUpper = upperCase.charAt(Math.floor(Math.random() * 26));
+var randomSymbol = symbols[(Math.floor(Math.random() * symbols.length))];
+var randomNumber = Math.floor(Math.random() * 10);
+
+var password = "";
